b6027d93de5a200b25bb9c60ddc6efd7fc02dffc
edson-alexandre <edson.ass@gmail.com>
2021-11-14 10:32:07 -0300

    Criação do componente TaskList

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import TaskList from './src/screens/TaskList';
 
-AppRegistry.registerComponent(appName, () => App);
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => TaskList);
---
+++ src/screens/TaskList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class TaskList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Lista de Tarefas</Text>
+      </View>
+    );
+  }
+}
