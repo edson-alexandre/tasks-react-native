5ec804fe2653c9815a730eb11bf6d281e09a9fcd
edson-alexandre <edson.ass@gmail.com>
2021-11-14 11:46:28 -0300

    Evoluindo o componente Task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,61 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import commomStykes from '../commomStykes';
 
 export default props => {
   return (
-    <View>
-      <Text> {props.desc} </Text>
-      <Text> {props.estimatedAt + ''} </Text>
-      <Text> {props.doneAt + ''} </Text>
+    <View style={styles.container}>
+      <View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
+      <View>
+        <Text> {props.desc} </Text>
+        <Text> {props.estimatedAt + ''} </Text>
+      </View>
     </View>
   );
 };
+
+function getCheckView(doneAt) {
+  if (doneAt !== null) {
+    return (
+      <View style={styles.done}>
+        <Icon name="check" size={20} color="#FFF" />
+      </View>
+    );
+  } else {
+    return <View style={styles.pendding}></View>;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    borderColor: '#AAA',
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 0,
+  },
+  checkContainer: {
+    width: '20%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 0,
+  },
+  pendding: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    borderWidth: 1,
+    borderColor: '#555',
+  },
+  done: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    backgroundColor: '#4d7031',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
